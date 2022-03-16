cf753eda38c59187bfe7792dc0b8ab581a72d578
abuanzeh <mohammed.abuanzeh@technzone.com>
2022-03-16 16:33:27 +0300

    under dev

--- app/src/main/java/com/technzone/firstlibrary/MainActivity.kt
+++ app/src/main/java/com/technzone/firstlibrary/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        ToasterMessage.showErrorMessage(this , "ERROR")
+        ToasterMessage.showSuccessMessage(this , "SUCCESS")
     }
 }
--- toasterLibrary/src/main/java/com/technzone/toasterlibrary/ToasterMessage.kt
+++ toasterLibrary/src/main/java/com/technzone/toasterlibrary/ToasterMessage.kt
@@ -1,13 +1,49 @@
 package com.technzone.toasterlibrary
 
 import android.content.Context
+import android.graphics.Color
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
+
 
 open class ToasterMessage {
 
     companion object {
-        fun show(context: Context, message: String) {
-            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        fun showErrorMessage(context: Context, message: String) {
+            val toast = Toast(context)
+            val view: View = LayoutInflater.from(context).inflate(R.layout.custem_toast_view, null)
+            val textView = view.findViewById(R.id.tvToast) as TextView
+            textView.text = message
+            toast.view = view
+            toast.setGravity(
+                Gravity.FILL_HORIZONTAL or Gravity.BOTTOM,
+                0,
+                0
+            )
+            toast.duration = Toast.LENGTH_LONG
+            toast.show()
         }
+
+        fun showSuccessMessage(context: Context, message: String) {
+            val toast = Toast(context)
+            val view: View = LayoutInflater.from(context).inflate(R.layout.custem_toast_view, null)
+            val textView = view.findViewById(R.id.tvToast) as TextView
+            val linearLayout = view.findViewById(R.id.LinearLayout) as LinearLayout
+            linearLayout.setBackgroundColor(Color.parseColor("#4CAF50"))
+            textView.text = message
+            toast.view = view
+            toast.setGravity(
+                Gravity.FILL_HORIZONTAL or Gravity.BOTTOM,
+                0,
+                0
+            )
+            toast.duration = Toast.LENGTH_LONG
+            toast.show()
+        }
+
     }
 }
