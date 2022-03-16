37ff50903be1ea331f3561f7f5776d56d4c63e19
abuanzeh <mohammed.abuanzeh@technzone.com>
2022-03-16 11:49:13 +0300

    under dev

--- toasterLibrary/src/main/java/com/technzone/toasterlibrary/ToasterMessage.kt
+++ toasterLibrary/src/main/java/com/technzone/toasterlibrary/ToasterMessage.kt
@@ -3,10 +3,10 @@ package com.technzone.toasterlibrary
 import android.content.Context
 import android.widget.Toast
 
-class ToasterMessage {
+open class ToasterMessage {
 
     companion object {
-        private fun show(context: Context, message: String) {
+        fun show(context: Context, message: String) {
             Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
         }
     }
