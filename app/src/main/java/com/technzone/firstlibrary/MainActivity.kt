7036979b32e1ab0cf79c85c79c7b7b74a9d86b91
abuanzeh <mohammed.abuanzeh@technzone.com>
2022-03-16 12:00:56 +0300

    under dev

--- app/src/main/java/com/technzone/firstlibrary/MainActivity.kt
+++ app/src/main/java/com/technzone/firstlibrary/MainActivity.kt
@@ -2,6 +2,7 @@ package com.technzone.firstlibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.technzone.toasterlibrary.ToasterMessage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
